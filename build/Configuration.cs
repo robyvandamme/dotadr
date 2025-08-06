c8998b27cde113ffb44c05ade1ca915c6d137527
Roby Van Damme <123795+robyvandamme@users.noreply.github.com>
2025-08-06 11:21:45 +0700

    config: setup solution (#1)
    Setup initial solution.
    
    * Add and configure solution and projects.
    * Add and configure build project including versioning and publishing.
    * Add initial GitHub workflows
    * Add license
    
    Closes #2

---
+++ build/Configuration.cs
@@ -0,0 +1,16 @@
+// Copyright © 2025 Roby Van Damme.
+
+using System.ComponentModel;
+using Nuke.Common.Tooling;
+
+[TypeConverter(typeof(TypeConverter<Configuration>))]
+public class Configuration : Enumeration
+{
+    public static Configuration Debug = new Configuration { Value = nameof(Debug) };
+    public static Configuration Release = new Configuration { Value = nameof(Release) };
+
+    public static implicit operator string(Configuration configuration)
+    {
+        return configuration.Value;
+    }
+}
---
+++ test/DotAdr.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+// Copyright © 2025 Roby Van Damme.
+
+namespace DotAdr.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
