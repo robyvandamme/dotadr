a0212f995dbfb5b6af0e2d8a500b6166b02a5e7c
Roby Van Damme <roby@robyvandamme.com>
2025-08-21 07:58:48 +0700

    fix: simplify settings inheritance

--- src/DotAdr/Commands/Init/InitAdrSettings.cs
+++ src/DotAdr/Commands/Init/InitAdrSettings.cs
@@ -5,7 +5,7 @@ using Spectre.Console.Cli;
 
 namespace DotAdr.Commands.Init;
 
-internal class InitAdrSettings : LogSettings
+internal class InitAdrSettings : AdrSettings
 {
     [Description("The directory to initialize.")]
     [CommandOption("-d|--directory")]
